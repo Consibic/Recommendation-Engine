92a31679fed8d71340972540c6801ea6d2dd7e97
Consibic <Consibic@users.noreply.github.com>
2015-09-19 23:46:22 -0400

    Create Client.java
    An application to simulate the recommendation engine.

---
+++ Client.java
@@ -0,0 +1,17 @@
+package project;
+
+public class Client {
+    
+    /**
+     * This is an application that simulates a book recommendation engine.
+     * The recommendation engine collects information of rating from users,
+     * and recommend three new books to the user based on their preference.
+     * The program uses standard databases created by developers.
+     * This engine has Graphical User Interfaces for user.
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GUIUserOpen open = new GUIUserOpen(true);
+                            //to start the engine from log in interface
+    }
+}
